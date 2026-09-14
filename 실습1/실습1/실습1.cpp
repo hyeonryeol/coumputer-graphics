f343fe6a7cc8ae9a08ab74e00d9426fc86375cbc
hyeonryeol <hunyeol138@gmail.com>
2026-09-14 17:19:38 +0900

    실습2 하는중

---
+++ 실습1/실습1/실습1.cpp
@@ -0,0 +1,99 @@
+﻿#include <GL/glew.h>
+#include <GL/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+//--- 함수 선언 (main보다 아래에 정의된 함수는 미리 선언해야 호출 가능)
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+float r = 1;
+float g = 1;
+float b = 1;
+bool   timeron = false;  // 타이머 켜짐 여부
+double lastChange = 0.0;    // 마지막으로 색 바꾼 시각(초)
+double interval = 0.5;    // 몇 초마다 바꿀지
+
+int main()
+{
+	// main 시작 부분에서 한 번만 호출
+	srand((unsigned)time(NULL));
+
+	
+	//--- GLFW 초기화
+	if (!glfwInit())
+		return -1;
+	//--- OpenGL 버전 설정
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+	//--- 윈도우 생성
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Keyboard Input", nullptr, nullptr);
+	//--- 컨텍스트 활성화
+	glfwMakeContextCurrent(window);
+	//--- GLEW 초기화
+	glewExperimental = GL_TRUE;
+	glewInit();
+	//--- 뷰포트 설정
+	glViewport(0, 0, 800, 600);
+	//--- 키보드 입력 처리 함수
+	glfwSetKeyCallback(window, KeyCallback); //--- 콜백 함수 등록
+	//--- 메인 루프
+	while (!glfwWindowShouldClose(window)) {
+		if (timeron)
+		{
+			double now = glfwGetTime();
+			if (now - lastChange >= interval)
+			{
+				r = rand() / (float)RAND_MAX;
+				g = rand() / (float)RAND_MAX;
+				b = rand() / (float)RAND_MAX;
+				lastChange = now;
+			}
+		}
+
+
+		//--- 화면 그리기
+		glClearColor(r, g, b, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+//--- End of Main function
+//--- 키보드 입력 처리 함수
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) //--- 등록한 사용자 정의 콜백 함수
+{
+	if (action == GLFW_PRESS)
+	{
+		switch (key) {
+		case GLFW_KEY_ESCAPE: glfwSetWindowShouldClose(window, GLFW_TRUE); break;
+		case GLFW_KEY_C: r = 0; g = 1; b = 1;
+
+			break;
+		case GLFW_KEY_M:;
+			r = 1; g = 0; b = 1;
+			break;
+		case GLFW_KEY_Y:;
+			r = 1; g = 1; b = 0;
+			break;
+		case GLFW_KEY_A:;
+			r = rand() / (float)RAND_MAX; g = rand() / (float)RAND_MAX; b = rand() / (float)RAND_MAX;
+			break;
+		case GLFW_KEY_G:;
+			r = 0.5; g = 0.5; b = 0.5;
+			break;
+		case GLFW_KEY_K:;
+			r = 0; g = 0; b = 0;
+			break;
+		case GLFW_KEY_T:;
+			timeron = true;
+			lastChange = glfwGetTime();
+			break;
+		case GLFW_KEY_S:;
+			timeron = false;
+			break;
+		}
+	}
+}
---
+++ 실습1/실습1/실습2.cpp
@@ -0,0 +1,232 @@
+﻿#include <GL/glew.h>
+#include <GL/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+//--- 함수 선언
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+struct Rect {
+	float x1, y1, x2, y2;   // 좌측하단, 우측상단
+	float r, g, b;          // 색
+};
+
+Rect rects[4][5];      // rects[사분면][몇 번째]
+int  count[4] = {};    // 사분면별 개수
+
+// 사분면 중심점
+float centerX[4] = { -0.5f,  0.5f, -0.5f, 0.5f };
+float centerY[4] = { 0.5f,  0.5f, -0.5f, -0.5f };
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	//--- GLFW 초기화
+	if (!glfwInit())
+		return -1;
+
+	//--- OpenGL 버전 설정
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+
+	//--- 윈도우 생성
+	GLFWwindow* window = glfwCreateWindow(800, 600, "실습2", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "윈도우 생성 실패!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	//--- 컨텍스트 활성화
+	glfwMakeContextCurrent(window);
+
+	//--- GLEW 초기화
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "GLEW 초기화 실패!" << std::endl;
+		return -1;
+	}
+
+	//--- 뷰포트 설정
+	glViewport(0, 0, 800, 600);
+
+	//--- 콜백 함수 등록
+	glfwSetKeyCallback(window, KeyCallback);
+	glfwSetMouseButtonCallback(window, MouseButtonCallback);
+
+	//--- 메인 루프
+	while (!glfwWindowShouldClose(window)) {
+		//--- 화면 그리기
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glColor3f(1.0f, 0.0f, 0.0f);
+		glRectf(-1.0f, 1.0f, 0, 0);
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glRectf(0, 1.0f, 1.0f, 0);
+
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glRectf(-1.0f, 0, 0, -1.0f);
+
+		glColor3f(0.0f, 1.0f, 1.0f);
+		glRectf(0, 0, 1.0f, -1.0f);
+
+		for (int q = 0; q < 4; ++q)
+		{
+			for (int i = 0; i < count[q]; ++i)
+			{
+				Rect& rc = rects[q][i];
+				glColor3f(rc.r, rc.g, rc.b);
+				glRectf(rc.x1, rc.y1, rc.x2, rc.y2);
+			}
+		}
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
+
+//--- 키보드 콜백
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (action == GLFW_PRESS)
+	{
+		switch (key) {
+		case GLFW_KEY_Q:
+			glfwSetWindowShouldClose(window, GLFW_TRUE);
+			break;
+		case GLFW_KEY_1:   // 왼쪽 위
+		{
+			int q = 0;
+			if (count[q] < 5)
+			{
+				Rect& rc = rects[q][count[q]];
+
+				float hw = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반폭
+				float hh = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반높이
+
+				//--- 중심 흔들기
+				float ox = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hw);
+				float oy = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hh);
+				float cx = centerX[q] + ox;
+				float cy = centerY[q] + oy;
+
+				rc.x1 = cx - hw;
+				rc.y1 = cy - hh;
+				rc.x2 = cx + hw;
+				rc.y2 = cy + hh;
+
+				rc.r = rand() / (float)RAND_MAX;
+				rc.g = rand() / (float)RAND_MAX;
+				rc.b = rand() / (float)RAND_MAX;
+
+				count[q] += 1;
+			}
+			break;
+		}
+		case GLFW_KEY_2:   // 오른쪽 위
+		{
+			int q = 1;
+			if (count[q] < 5)
+			{
+				Rect& rc = rects[q][count[q]];
+
+				float hw = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반폭
+				float hh = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반높이
+
+				//--- 중심 흔들기
+				float ox = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hw);
+				float oy = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hh);
+				float cx = centerX[q] + ox;
+				float cy = centerY[q] + oy;
+
+				rc.x1 = cx - hw;
+				rc.y1 = cy - hh;
+				rc.x2 = cx + hw;
+				rc.y2 = cy + hh;
+
+				rc.r = rand() / (float)RAND_MAX;
+				rc.g = rand() / (float)RAND_MAX;
+				rc.b = rand() / (float)RAND_MAX;
+
+				count[q] += 1;
+			}
+			break;
+		}
+		case GLFW_KEY_3:   // 왼쪽 아래
+		{
+			int q = 2;
+			if (count[q] < 5)
+			{
+				Rect& rc = rects[q][count[q]];
+
+				float hw = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반폭
+				float hh = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반높이
+
+				//--- 중심 흔들기
+				float ox = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hw);
+				float oy = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hh);
+				float cx = centerX[q] + ox;
+				float cy = centerY[q] + oy;
+
+				rc.x1 = cx - hw;
+				rc.y1 = cy - hh;
+				rc.x2 = cx + hw;
+				rc.y2 = cy + hh;
+
+				rc.r = rand() / (float)RAND_MAX;
+				rc.g = rand() / (float)RAND_MAX;
+				rc.b = rand() / (float)RAND_MAX;
+
+				count[q] += 1;
+			}
+			break;
+		}
+		case GLFW_KEY_4:   // 오른쪽 아래
+		{
+			int q = 3;
+			if (count[q] < 5)
+			{
+				Rect& rc = rects[q][count[q]];
+
+				float hw = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반폭
+				float hh = 0.08f + (rand() / (float)RAND_MAX) * 0.12f;  // 반높이
+
+				//--- 중심 흔들기
+				float ox = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hw);
+				float oy = (rand() / (float)RAND_MAX * 2.0f - 1.0f) * (0.45f - hh);
+				float cx = centerX[q] + ox;
+				float cy = centerY[q] + oy;
+
+				rc.x1 = cx - hw;
+				rc.y1 = cy - hh;
+				rc.x2 = cx + hw;
+				rc.y2 = cy + hh;
+
+				rc.r = rand() / (float)RAND_MAX;
+				rc.g = rand() / (float)RAND_MAX;
+				rc.b = rand() / (float)RAND_MAX;
+
+				count[q] += 1;
+			}
+			break;
+		}
+		}
+	}
+}
+//--- 마우스 버튼 콜백
+void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+	{
+		double x, y;
+		glfwGetCursorPos(window, &x, &y);
+	}
+}
